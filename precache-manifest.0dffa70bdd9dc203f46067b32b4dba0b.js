2652aaa3d9cbddd7233e3b06843dfbe90adee761
Roxxy Rafael <roxxydev@gmail.com>
2020-08-23 20:13:29 +0800

    Updates

---
+++ precache-manifest.0dffa70bdd9dc203f46067b32b4dba0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a1ebcb09122a88c778d1fa953cd7b3",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "23f4a9d2f57de4d35884",
+    "url": "/todo-app-react/static/css/main.64641290.chunk.css"
+  },
+  {
+    "revision": "ce9875f31fd81650c58e",
+    "url": "/todo-app-react/static/js/2.249e460d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app-react/static/js/2.249e460d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f4a9d2f57de4d35884",
+    "url": "/todo-app-react/static/js/main.3f4a7175.chunk.js"
+  },
+  {
+    "revision": "85cae369ffabab9cc3e3",
+    "url": "/todo-app-react/static/js/runtime-main.e0d73842.js"
+  }
+]);
